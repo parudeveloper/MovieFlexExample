8997d7c011c6451a0cd4902d3e202f9b32333875
parudeveloper <paramesh.developer@gmail.com>
2024-11-19 17:35:40 +0530

    Updated Code

--- app/src/main/java/com/movieflex/MainActivity.kt
+++ app/src/main/java/com/movieflex/MainActivity.kt
@@ -5,14 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
 import com.movieflex.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment).navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    override fun onBackPressed() {
+        if (navController.currentDestination?.id == R.id.splashFragment) {
+            finish()
+        }
+        super.onBackPressed()
     }
 }
